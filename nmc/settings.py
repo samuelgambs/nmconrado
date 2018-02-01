5de88da59c145fb9a0469710dd34fd079b40122d
Samuel Dias <samuelcddo@gmail.com>
2018-02-01 14:14:45 -0200

    settings

--- nmc/settings.py
+++ nmc/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/bitnami/static/'
+STATIC_ROOT = '/home/bitnami/nmconrado/nmcapp/static/'
 
 TINYMCE_DEFAULT_CONFIG = {'theme': 'simple', 'width': '625', 'relative_urls': False}
